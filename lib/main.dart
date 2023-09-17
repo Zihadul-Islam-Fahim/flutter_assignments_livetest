33d6b196d6fb71f0f29e5527c1626a9162941d60
Mr. Fahim <fahimzihad@gmail.com>
2023-09-17 18:59:42 +0600

    Adding Gridview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+<<<<<<< HEAD
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Gallery'),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              'Welcome To My Photo Gallery!',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Search for photos..."),
+            ),
+          ),
+
+          GridView(
+
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            children: [
+              ListTile(leading: Icon(Icons.ad_units_sharp),),
+              ListTile(leading: Icon(Icons.ad_units_sharp),),
+              ListTile(leading: Icon(Icons.ad_units_sharp),),
+              ListTile(leading: Icon(Icons.ad_units_sharp),),
+            ],
+          ),
+
+
+        ],
+      ),
+    );
+  }
+}
+=======
  class MyApp extends StatelessWidget {
    const MyApp({super.key});
 
@@ -35,3 +98,4 @@ void main() => runApp(MyApp());
      );
    }
  }
+>>>>>>> origin/Just_test
