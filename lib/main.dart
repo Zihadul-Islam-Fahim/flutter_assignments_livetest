065c45682009765298b844e29a8a31f9da489918
Mr. Fahim <fahimzihad@gmail.com>
2023-09-23 13:15:08 +0600

    Assignment 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,148 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-<<<<<<< HEAD
+main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class Product {
+  String name = '';
+  double? price= 0;
+  int? count = 0;
+
+  Product(this.name, this.price, this.count);
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeActivity(),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
 
-class HomeActivity extends StatelessWidget {
-  const HomeActivity({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List productlist = [
+    Product('Monitor', 250, 0),
+    Product('Mouse', 20, 0),
+    Product('Keyboard', 50, 0),
+    Product('Mouse Pad', 5, 0),
+    Product('Book', 20, 0),
+    Product('Cofee Mug', 5, 0),
+    Product('Earpods', 70, 0),
+    Product('Mobile Cover', 15, 0),
+    Product('Sunglass', 20, 0),
+    Product('Umbrella', 5, 0),
+    Product('Router', 50, 0),
+    Product('Coffee Mug', 5, 0),
+    Product('Soundbox', 100, 0),
+    Product('Calculator', 30, 0),
+  ];
+  int? cart = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Photo Gallery'),
+        title: const Text('Product List'),
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Text(
-              'Welcome To My Photo Gallery!',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.shopping_cart),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Cartpage(cartcount: cart)));
+        },
+      ),
+      body: ListView.builder(
+          itemCount: productlist.length,
+          itemBuilder: (context, int i) {
+            Product product = productlist[i];
+            return ListTile(
+              title: Text(product.name),
+              subtitle: Text('\$${product.price}'),
+              trailing: Column(
+                children: [
+                  Text('Count: ${product.count}'),
+                  Expanded(
+                      child: ElevatedButton(
+                    child: const Text(
+                      'Buy Now',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        product.count = product.count! + 1;
+                        if (product.count == 5) {
+                          setState(() {
+                            cart = cart! + 1;
+                          });
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text('Congratulations !'),
+                                  content:
+                                      Text('You\'ve bought 5 ${product.name}!'),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text('Ok'))
+                                  ],
+                                );
+                              });
+                        } else if (product.count == 6) {
+                          product.count = 5;
+                        }
+                      });
+                    },
+                  ))
+                ],
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Search for photos..."),
-            ),
-          ),
+            );
+          }),
+    );
+  }
+}
 
-          GridView(
+class Cartpage extends StatefulWidget {
+  final int? cartcount;
 
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-            children: [
-              ListTile(leading: Icon(Icons.ad_units_sharp),),
-              ListTile(leading: Icon(Icons.ad_units_sharp),),
-              ListTile(leading: Icon(Icons.ad_units_sharp),),
-              ListTile(leading: Icon(Icons.ad_units_sharp),),
-            ],
-          ),
+  const Cartpage({super.key, required this.cartcount});
 
+  @override
+  State<Cartpage> createState() => _CartpageState();
+}
 
-        ],
+class _CartpageState extends State<Cartpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Total Product ${widget.cartcount}',style: const TextStyle(fontSize: 26),),
       ),
     );
   }
 }
-=======
- class MyApp extends StatelessWidget {
-   const MyApp({super.key});
-
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: HomeActivity(),
-     );
-   }
- }
- class HomeActivity extends StatelessWidget {
-   const HomeActivity({super.key});
-
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(
-         title: Text("My Shopping List"),
-         actions: [Icon(Icons.shopping_cart)],
-         centerTitle: true,
-       ),
-       body: ListView(
-         children: [
-           ListTile(leading: Icon(Icons.apple,),title: Text("Apples"),),
-           ListTile(leading: Icon(Icons.access_time_filled_rounded,),title: Text("Watch"),),
-           ListTile(leading: Icon(Icons.add_a_photo,),title: Text("Camera"),),
-           ListTile(leading: Icon(Icons.account_balance_wallet_rounded,),title: Text("Wallet"),),
-           ListTile(leading: Icon(Icons.ad_units_sharp,),title: Text("Phone"),),
-         ],
-       ),
-     );
-   }
- }
->>>>>>> origin/Just_test
