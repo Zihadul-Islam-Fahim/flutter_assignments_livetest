9e4914e4c0b8f80f1a4ff8b497a5970c973078de
Mr. Fahim <fahimzihad@gmail.com>
2023-09-24 23:29:41 +0600

    Livetest 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_assignments_livetest/Cart_Page.dart';
 
 main() {
   runApp(const MyApp());
-}
-
-class Product {
-  String name = '';
-  double? price = 0;
-  int? count = 0;
-
-  Product(this.name, this.price, this.count);
 }
 
 class MyApp extends StatefulWidget {
@@ -39,91 +30,78 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List productlist = [
-    Product('Monitor', 250, 0),
-    Product('Mouse', 20, 0),
-    Product('Keyboard', 50, 0),
-    Product('Mouse Pad', 5, 0),
-    Product('Book', 20, 0),
-    Product('Cofee Mug', 5, 0),
-    Product('Earpods', 70, 0),
-    Product('Mobile Cover', 15, 0),
-    Product('Sunglass', 20, 0),
-    Product('Umbrella', 5, 0),
-    Product('Router', 50, 0),
-    Product('Coffee Mug', 5, 0),
-    Product('Soundbox', 100, 0),
-    Product('Calculator', 30, 0),
-  ];
-  int? cart = 0;
+  int count = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product List'),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.shopping_cart),
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => Cartpage(cartcount: cart)));
-        },
-      ),
-      body: ListView.builder(
-          itemCount: productlist.length,
-          itemBuilder: (context, int i) {
-            Product product = productlist[i];
-            return Card(
-              child: ListTile(
-                title: Text(product.name),
-                subtitle: Text('\$${product.price}'),
-                trailing: Column(
-                  children: [
-                    Text('Count: ${product.count}'),
-                    Expanded(
-                        child: ElevatedButton(
-                      child: const Text(
-                        'Buy Now',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
+        appBar: AppBar(
+          title: const Text('Counter App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Count : ',
+                style: TextStyle(fontSize: 20),
+              ),
+              Text('$count',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
                       onPressed: () {
                         setState(() {
-                          product.count = product.count! + 1;
-                          if (product.count == 5) {
-                            setState(() {
-                              cart = cart! + 1;
-                            });
+                          if (count <= 0) {
+                            count == 0;
+                          } else {
+                            count--;
+                          }
+                        });
+                      },
+                      child: Text(
+                        '-',
+                        style: TextStyle(fontSize: 25),
+                      )),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          count++;
+                          if (count >= 5) {
                             showDialog(
                                 context: context,
                                 builder: (context) {
                                   return AlertDialog(
-                                    title: const Text('Congratulations !'),
-                                    content: Text(
-                                        'You\'ve bought 5 ${product.name}!'),
+                                    title: Text("Button Pressed $count times"),
                                     actions: [
                                       TextButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                          child: const Text('Ok'))
+                                        child: Text(
+                                          'Close',
+                                          style: TextStyle(color: Colors.blue),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                      )
                                     ],
                                   );
                                 });
-                          } else if (product.count == 6) {
-                            product.count = 5;
                           }
                         });
                       },
-                    ))
-                  ],
-                ),
-              ),
-            );
-          }),
-    );
+                      child: Text(
+                        '+',
+                        style: TextStyle(fontSize: 25),
+                      ))
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
