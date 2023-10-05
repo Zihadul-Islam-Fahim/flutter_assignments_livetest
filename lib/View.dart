b62e350a6ae8d9c0bd41cfa3fabfbb8a0ccc337d
Mr. Fahim <fahimzihad@gmail.com>
2023-10-06 01:07:36 +0600

    Assignment 9

--- lib/View.dart
+++ lib/View.dart
@@ -1,38 +1,264 @@
 import 'package:flutter/material.dart';
-
+import 'package:responsive_builder/responsive_builder.dart';
 import 'main.dart';
 
-Widget protrait(context) {
-  return ListView.separated(
-      itemBuilder: (context, index) {
-        return Card(
-          child: SizedBox(
-              height: 150,
-              child: Image.network(
-                imageList[index],
-              )),
-        );
-      },
-      separatorBuilder: (context, index) {
-        return SizedBox(
-          height: 8,
-        );
-      },
-      itemCount: imageList.length);
+class HomePage extends StatefulWidget {
+  HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
-Widget landScape(context) {
-  return Card(
-    child: GridView.builder(
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (context, index) {
-          return Card(
-            child: Image.network(
-              imageList[index],
-              alignment: Alignment.topCenter,
+class _HomePageState extends State<HomePage> {
+  List imageList = [
+    'https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80',
+    'https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1898&q=80',
+    'https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+    'https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80',
+  ];
+  List<Products> ProductList = [
+    Products('Luis Fris', 'Orange', 1, 42, 50,
+        'https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80'),
+    Products('Air Jordan 12 Gym Red', 'Red', 1, 40, 100,
+        'https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'),
+    Products('Heiso SL', 'Black', 1, 39, 70,  'https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1780&q=80')
+  ];
+  int totaloneItem = 0;
+  int totalShopping = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(
+            alignment: AlignmentDirectional.topEnd,
+            child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  size: 40,
+                )),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'My Shoe',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
             ),
-          );
-        }),
-  );
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: ProductList.length,
+                itemBuilder: (context, index) {
+                  Products product = ProductList[index];
+                  return Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Container(
+                      clipBehavior: Clip.hardEdge,
+                      height: 120,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: [BoxShadow(blurRadius: 0.2)]),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 16 / 13,
+                            child: ClipRect(
+                              child: Image.network(product.productPhoto),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 40,
+                            child: Padding(
+                              padding: EdgeInsets.all(8),
+                              child: SizedBox(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Expanded(
+                                      flex: 25,
+                                      child: Text(
+                                        '${product.productName}',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 15,
+                                      child: RichText(
+                                        text: TextSpan(
+                                          children: [
+                                            TextSpan(
+                                              text: 'Color: ',
+                                              style:
+                                                  TextStyle(color: Colors.grey),
+                                            ),
+                                            TextSpan(
+                                              text:
+                                                  '${product.productColor}    ',
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            ),
+                                            TextSpan(
+                                              text: 'Size: ',
+                                              style:
+                                                  TextStyle(color: Colors.grey),
+                                            ),
+                                            TextSpan(
+                                              text:
+                                                  '${product.productSize}    ',
+                                              style: TextStyle(
+                                                  color: Colors.black),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 60,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Expanded(
+                                            flex: 40,
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                right: 9,
+                                                top: 5,
+                                              ),
+                                              child: Container(
+                                                height: 45,
+                                                child: IconButton(
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      if (product.productCount >
+                                                          1) {
+                                                        product.productCount--;
+                                                        totaloneItem -= product
+                                                            .productValue;
+                                                        totalShopping -=
+                                                            totaloneItem;
+                                                      } else {
+                                                        product.productCount =
+                                                            1;
+                                                      }
+                                                    });
+                                                  },
+                                                  icon: Icon(Icons.remove),
+                                                ),
+                                                decoration: BoxDecoration(
+                                                    color: Colors.white,
+                                                    boxShadow: [
+                                                      BoxShadow(blurRadius: 1)
+                                                    ],
+                                                    shape: BoxShape.circle),
+                                              ),
+                                            ),
+                                          ),
+                                          Expanded(
+                                              flex: 20,
+                                              child: Text(
+                                                  '${product.productCount}',
+                                                  style:
+                                                      TextStyle(fontSize: 21))),
+                                          Expanded(
+                                            flex: 40,
+                                            child: Padding(
+                                              padding: EdgeInsets.only(
+                                                  top: 5, right: 9),
+                                              child: Container(
+                                                height: 45,
+                                                child: IconButton(
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      product.productCount++;
+                                                      totaloneItem +=
+                                                          product.productValue;
+                                                      totalShopping +=
+                                                          totaloneItem;
+                                                    });
+                                                  },
+                                                  icon: Icon(Icons.add),
+                                                ),
+                                                decoration: BoxDecoration(
+                                                    color: Colors.white,
+                                                    boxShadow: [
+                                                      BoxShadow(blurRadius: 1)
+                                                    ],
+                                                    shape: BoxShape.circle),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 15,
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: SizedBox(
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Icon(Icons.more_vert),
+                                      Text(
+                                        '${product.productValue}\$',
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          SizedBox(
+            child: Column(
+              children: [Text('$totalShopping')],
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+class Products {
+  final productName;
+  final productColor;
+  int productCount;
+
+  final productSize;
+  int productValue;
+  final productPhoto;
+
+  Products(this.productName, this.productColor, this.productCount,
+      this.productSize, this.productValue, this.productPhoto);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,42 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_assignments_livetest/View.dart';
 
-main() => runApp(const MyApp());
+
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+import 'View.dart';
+
+main() => runApp( MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.cyan),
-      themeMode: ThemeMode.light,
-      home: const HomePage(),
+    return DevicePreview(
+      builder: (BuildContext context) => ResponsiveApp(
+        builder: (BuildContext) {
+          double widesize = MediaQuery.of(context).size.width;
+          bool? bigScreen = m(widesize);
+            return MaterialApp(
+              themeMode: ThemeMode.system,
+              theme: ThemeData(primarySwatch: Colors.yellow),
+              // useInheritedMediaQuery: true,
+              locale: DevicePreview.locale(context),
+              builder: DevicePreview.appBuilder,
+              home:  HomePage(),
+            );
+
+        },
+      ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) =>
-      OrientationBuilder(builder: (context, orientation) {
-        final isProtrait = orientation == Orientation.portrait;
-        return Scaffold(
-            appBar: AppBar(
-              title: const Text("News Feed"),
-            ),
-            body: isProtrait ? protrait(context) : landScape(context));
-      });
+ m(widesize){
+  if(widesize>600){
+    return true;
+  }
 }
 
 List imageList = [
