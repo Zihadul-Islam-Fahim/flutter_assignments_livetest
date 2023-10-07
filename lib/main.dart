518153409265f7f9113052a35dadb52d4dee6254
Zihadul-Islam-Fahim <82943440+Zihadul-Islam-Fahim@users.noreply.github.com>
2023-10-07 15:30:54 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,15 @@
-
-
-import 'package:responsive_builder/responsive_builder.dart';
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
-
 import 'View.dart';
 
-main() => runApp( MyApp());
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DevicePreview(
-      builder: (BuildContext context) => ResponsiveApp(
-        builder: (BuildContext) {
-          double widesize = MediaQuery.of(context).size.width;
-          bool? bigScreen = m(widesize);
-            return MaterialApp(
-              themeMode: ThemeMode.system,
-              theme: ThemeData(primarySwatch: Colors.yellow),
-              // useInheritedMediaQuery: true,
-              locale: DevicePreview.locale(context),
-              builder: DevicePreview.appBuilder,
-              home:  HomePage(),
-            );
-
-        },
-      ),
+    return MaterialApp(
+      home: HomePage(),
     );
   }
 }
