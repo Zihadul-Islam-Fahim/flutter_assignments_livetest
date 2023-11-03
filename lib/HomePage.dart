aeaec4c50bb0027ab646e7111e133314029ba463
Mr. Fahim <fahimzihad@gmail.com>
2023-11-03 23:24:02 +0600

    Livetest 11

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,105 +1,64 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_assignments_livetest/ShowDetails.dart';
-import 'package:http/http.dart' as http;
+import 'WeatherData.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  List<Photos> photoList = [];
-
-  Future<List<Photos>> getPhotosApi() async {
-    var response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
-    var data = jsonDecode(response.body.toString());
-    if (response.statusCode == 200) {
-      for (Map i in data) {
-        Photos photos = Photos(id: i["id"], title: i["title"], url: i["url"]);
-        photoList.add(photos);
-        print(photos.id);
-      }
-      return photoList;
-    } else {
-      return photoList;
-    }
-  }
-
-  void initState() {
-    getPhotosApi();
-    super.initState();
-  }
+class WeatherInfoScreen extends StatelessWidget {
+  final List<WeatherData> weatherData = [
+    WeatherData(
+      city: "New York",
+      temperature: 20,
+      condition: "Clear",
+      humidity: 60,
+      windSpeed: 5.5,
+    ),
+    WeatherData(
+      city: "Los Angeles",
+      temperature: 25,
+      condition: "Sunny",
+      humidity: 50,
+      windSpeed: 6.8,
+    ),
+    WeatherData(
+      city: "London",
+      temperature: 15,
+      condition: "Partly Cloudy",
+      humidity: 70,
+      windSpeed: 4.2,
+    ),
+    WeatherData(
+      city: "Tokyo",
+      temperature: 28,
+      condition: "Rainy",
+      humidity: 75,
+      windSpeed: 8.0,
+    ),
+    WeatherData(
+      city: "Sydney",
+      temperature: 22,
+      condition: "Cloudy",
+      humidity: 55,
+      windSpeed: 7.3,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Photo Gallery Album"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: FutureBuilder(
-              future: getPhotosApi(),
-              builder:
-                  (BuildContext context, AsyncSnapshot<List<Photos>> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListView.separated(
-                      itemCount: photoList.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          leading: SizedBox(
-                              height: 60,
-                              width: 60,
-                              child: Image.network(
-                                '${snapshot.data![index].url}',
-                                fit: BoxFit.cover,
-                              )),
-                          title: Text(snapshot.data![index].title.toString()),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ShowDetails(
-                                        photoIndex: photoList[index])));
-                          },
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return SizedBox(
-                          height: 8,
-                        );
-                      },
-                    ),
-                  );
-                }
-              },
-            ),
+    return ListView.builder(
+      itemCount: weatherData.length,
+      itemBuilder: (context, index) {
+        final data = weatherData[index];
+        return ListTile(
+          title: Text(data.city),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Temperature: ${data.temperature}°C'),
+              Text('Condition: ${data.condition}'),
+              Text('Humidity: ${data.humidity}%'),
+              Text('Wind Speed: ${data.windSpeed} km/h'),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
-}
-
-class Photos {
-  final int id;
-  final String title;
-  final String url;
-
-  Photos({
-    required this.id,
-    required this.title,
-    required this.url,
-  });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'HomePage.dart';
 
-main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Weather Info App'),
+        ),
+        body: WeatherInfoScreen(),
+      ),
     );
   }
 }
-
